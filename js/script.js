0381258cbe14bbd1d59804678bd43a49a7a3aaf0
Qadri Aditya <qadriaditya6@gmail.com>
2025-07-20 01:25:13 +0700

    update bagian menu
    menambah kan css dan menu-menu yang tersedia

--- js/script.js
+++ js/script.js
@@ -3,16 +3,28 @@ console.log("Script loaded");
 // Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 const hamburger = document.querySelector("#hamburger-menu");
+const toggleBtn = document.getElementById("toggleMenu");
+const menuTambahan = document.getElementById("menuLainnya");
 
-// ketika hamburger menu diklik
+// Toggle ketika hamburger menu diklik
 hamburger.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan nav
+// klik di luar sidebar hamburger untuk menghilangkan nav
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
 });
 
+// Toggle Menu Lainnya
+toggleBtn.addEventListener("click", function () {
+  if (menuTambahan.style.display === "none") {
+    menuTambahan.style.display = "grid";
+    toggleBtn.textContent = "Sembunyikan";
+  } else {
+    menuTambahan.style.display = "none";
+    toggleBtn.textContent = "Menu Lainnya";
+  }
+});
